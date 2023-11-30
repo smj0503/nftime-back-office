5dab51f4790e7a6c8e64cf330f8d1124114dd903
smj0503 <wovj0503@gmail.com>
2023-12-01 02:25:29 +0900

    [Test : Sony] Dummy action 삽입 중

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
+import { useRouter } from "next/router";
 import { useState } from "react";
+import Image from "next/image";
 
 import ActionButton from "@/components/ActionButton";
 
@@ -7,22 +9,31 @@ import Logo from "/public/assets/logo/logo-signin.svg";
 
 export default function ()
 {
+    const router = useRouter();
+
     const [id, setId] = useState('');
     const [password, setPassword] = useState('');
 
     const onIdChange = (e) =>
     {
         setId(e.target.value);
-    }
+    };
 
     const onPasswordChange = (e) =>
     {
         setPassword(e.target.value);
-    }
+    };
+
+    const onClick = async () =>
+    {
+        await router.push('/register/company');
+    };
 
     return (
         <div className={ styles.container }>
-            <div className={ styles.thumbnail }></div>
+            <div className={ styles.thumbnail }>
+                <Image src="/assets/photo/photo-login.png" width={683} height={702} alt="login image" priority={true}/>
+            </div>
             <div className={ styles.login }>
                 <div className={ styles.loginBox }>
                     <Logo/>
@@ -34,7 +45,7 @@ export default function ()
                         <input type="text" placeholder="ID" onChange={ onIdChange }/>
                         <input type="password" placeholder="Password" onChange={ onPasswordChange }/>
                     </div>
-                    <ActionButton width={322}>{ "Login" }</ActionButton>
+                    <ActionButton disabled={ !(!!id && !!password) } width={322} onClick={ onClick }>{ "Login" }</ActionButton>
                 </div>
             </div>
         </div>
