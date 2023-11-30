f1462031634dcbfda69b722b624108aafa13437a
smj0503 <wovj0503@gmail.com>
2023-12-01 01:07:43 +0900

    [Test : Sony] Dummy data 삽입 중

--- components/CompanyTable/index.js
+++ components/CompanyTable/index.js
@@ -24,7 +24,7 @@ export default function ()
                 </div>
             </div>
             <CompanyItem
-                image={}
+                image="/assets/dummies/company_protocolcamp.png"
                 name="Protocol Camp"
                 description="Protocol Camp is a program that develops real-world products related to blockchain. Anyone who is dreaming of a career in the blockchain ecosystem can apply. Be a member of the Web3.0 ecosystem community created by Protocol Camp to discover personal growth potential and draw a career blueprint in the blockchain industry."
                 certificateNum={1}
@@ -32,7 +32,7 @@ export default function ()
                 date="2023.11.20"
             />
             <CompanyItem
-                image={}
+                image="/assets/dummies/company_image_dreamplus academy.png"
                 name="Dreamplus Academy"
                 description="Dream Plus Academy aims to cultivate top experts in cybersecurity. It is an enterprising community where Dreamer, the organizer, Hanwha Life, and the global cybersecurity expert group, Tiori, work together to build an ecosystem."
                 certificateNum={1}
@@ -40,20 +40,20 @@ export default function ()
                 date="2023.11.15"
             />
             <CompanyItem
-                image={}
-                name="Dreamplus Academy"
-                description="Dream Plus Academy aims to cultivate top experts in cybersecurity. It is an enterprising community where Dreamer, the organizer, Hanwha Life, and the global cybersecurity expert group, Tiori, work together to build an ecosystem."
-                certificateNum={1}
-                issueNum={19}
-                date="2023.11.15"
-            />
-            <CompanyItem
-                image={}
+                image="/assets/dummies/company_ewha_university.png"
                 name="Ewha Womans University"
                 description="Korea's first female school and university. Its predecessor is Ewha Hakdang, founded in 1886, the fifth oldest institution of higher education in Korea."
                 certificateNum={1}
                 issueNum={19}
                 date="2023.11.15"
+            />
+            <CompanyItem
+                image="/assets/dummies/company_skku.png"
+                name="Sungkyunkwan University"
+                description="Korea's oldest institution of higher education in Asia with a history of 625 years"
+                certificateNum={1}
+                issueNum={32}
+                date="2023.11.05"
             />
         </div>
     )
