1edb97b034fc0a1bd0b7a9f2283f7f883373e58c
smj0503 <wovj0503@gmail.com>
2023-12-20 02:13:10 +0900

    [UI Design: Sony] font 설정

---
+++ pages/_documents.js
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class CustomDocument extends Document
+{
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="preconnect" href="https://cdn.jsdelivr.net" crossOrigin="anonymous"/>
+                    <link rel="preload" as="style" crossOrigin href="https://cdn.jsdelivr.net/gh/wanteddev/wanted-sans@v1.0.1/packages/wanted-sans/fonts/webfonts/variable/split/WantedSansVariable.min.css"/>
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/wanteddev/wanted-sans@v1.0.1/packages/wanted-sans/fonts/webfonts/variable/split/WantedSansVariable.min.css"/>
+                </Head>
+                <body data-theme="light">
+                <Main />
+                </body>
+                <NextScript />
+            </Html>
+        );
+    }
+}
