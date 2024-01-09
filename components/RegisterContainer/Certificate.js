adec5c40ac06f5b540973d4ae570bf92df2bd083
smj0503 <wovj0503@gmail.com>
2024-01-10 01:18:52 +0900

    [Locale: Sony] 한글화 작업 중

--- components/RegisterContainer/Certificate.js
+++ components/RegisterContainer/Certificate.js
@@ -1,7 +1,11 @@
+import useTranslation from "next-translate/useTranslation";
+
 import styles from "./RegisterContainer.module.css";
 
 export default function ({ setCompany, setCategory, setStartDate, setEndDate, setName, setDescription, setUrl, companyList })
 {
+    const { t } = useTranslation("common");
+
     const onChangeCompany = (e) =>
     {
         setCompany(e.target.value);
@@ -41,9 +45,9 @@ export default function ({ setCompany, setCategory, setStartDate, setEndDate, se
         <div className={ styles.container }>
             <div className={ styles.pair }>
                 <div className={ styles.item }>
-                    <label className={ styles.label }>{ "Choose Company" }</label>
+                    <label className={ styles.label }>{ t("register.certificate.companyName") }</label>
                     <select className={ styles.input } onChange={ onChangeCompany }>
-                        <option>{ "Choose Company" }</option>
+                        <option>{ t("register.certificate.chooseCompany") }</option>
                         {
                             companyList.map((company, index) => {
                                 return <option key={ index }>{ company.company_name }</option>
@@ -52,9 +56,9 @@ export default function ({ setCompany, setCategory, setStartDate, setEndDate, se
                     </select>
                 </div>
                 <div className={ styles.item }>
-                    <label className={ styles.label }>{ "Certificate Category" }</label>
+                    <label className={ styles.label }>{ t("register.certificate.certificateCategory") }</label>
                     <select className={ styles.input } onChange={ onChangeCategory }>
-                        <option>{ "Choose Category" }</option>
+                        <option>{ t("register.certificate.chooseCategory") }</option>
                         <option>{ "Diploma" }</option>
                         <option>{ "Contest" }</option>
                         <option>{ "License" }</option>
@@ -65,25 +69,25 @@ export default function ({ setCompany, setCategory, setStartDate, setEndDate, se
 
             <div className={ styles.pair }>
                 <div className={ styles.item }>
-                    <label className={ styles.label }>{ "Start date" }</label>
+                    <label className={ styles.label }>{ t("register.certificate.startDate") }</label>
                     <input type="text" className={ styles.input } onChange={ onChangeStartDate }/>
                 </div>
                 <div className={ styles.item }>
-                    <label className={ styles.label }>{ "End date" }</label>
+                    <label className={ styles.label }>{ t("register.certificate.endDate") }</label>
                     <input type="text" className={ styles.input } onChange={ onChangeEndDate }/>
                 </div>
             </div>
 
             <div className={ styles.item }>
-                <label className={ styles.label }>{ "Certificate name" }</label>
+                <label className={ styles.label }>{ t("register.certificate.name") }</label>
                 <textarea className={ styles.input } onChange={ onChangeName }/>
             </div>
             <div className={ styles.item }>
-                <label className={ styles.label }>{ "Certificate Website" }</label>
+                <label className={ styles.label }>{ t("register.certificate.website") }</label>
                 <textarea className={ styles.input } onChange={ onChangeUrl }/>
             </div>
             <div className={ styles.item } data-long={true}>
-                <label className={ styles.label }>{ "Certificate Description" }</label>
+                <label className={ styles.label }>{ t("register.certificate.description") }</label>
                 <textarea className={ styles.input } onChange={ onChangeDescription } data-long={true}/>
             </div>
         </div>
