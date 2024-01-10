8aa1afa8966caf466f3ee6eea94c493e2ba6fa13
smj0503 <wovj0503@gmail.com>
2024-01-10 11:22:42 +0900

    [Feature : Sony] http 통신

--- pages/_app.js
+++ pages/_app.js
@@ -9,8 +9,6 @@ export default function App({ Component, pageProps })
         <>
             <Head>
                 {/*<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1"/>*/}
-                {/*<meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests"/>*/}
-                <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
                 <title>CERTI Back Office</title>
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
