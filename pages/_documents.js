ac21f657e8813580b89c8b714563dfae05d397b3
smj0503 <wovj0503@gmail.com>
2024-01-14 23:57:39 +0800

    [BugFix : Sony]

--- pages/_documents.js
+++ pages/_documents.js
@@ -6,9 +6,9 @@ export default class CustomDocument extends Document
         return (
             <Html>
                 <Head>
-                    <link rel="preconnect" href="https://cdn.jsdelivr.net" crossOrigin="anonymous"/>
-                    {/*<link rel="preload" as="style" crossOrigin href="https://cdn.jsdelivr.net/gh/wanteddev/wanted-sans@v1.0.1/packages/wanted-sans/fonts/webfonts/variable/split/WantedSansVariable.min.css"/>*/}
-                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/wanteddev/wanted-sans@v1.0.1/packages/wanted-sans/fonts/webfonts/variable/split/WantedSansVariable.min.css"/>
+                    <link rel="preconnect" href="https://cdn.jsdelivr.net" crossOrigin="anonymous" />
+                    <link rel="preload" as="style" crossOrigin href="https://cdn.jsdelivr.net/gh/wanteddev/wanted-sans@v1.0.1/packages/wanted-sans/fonts/webfonts/variable/split/WantedSansVariable.min.css" />
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/wanteddev/wanted-sans@v1.0.1/packages/wanted-sans/fonts/webfonts/variable/split/WantedSansVariable.min.css" />
                 </Head>
                 <body data-theme="light">
                 <Main />
